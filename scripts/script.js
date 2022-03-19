a2fb05b70d471292c44c845e090b1f84efabaebb
Nathalie Daoût <nat.dao3@gmail.com>
2022-03-19 20:27:55 +0100

    background Pokedex

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,19 @@
 document.getElementById('run').addEventListener('click', async() => {
-    let input = document.getElementById('searchName').value.trim();
-    
-    let response = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=20&offset=20`);
-    let poke = await response.json();
-    console.log(poke);
+    let input = document.getElementById('search_input').value.trim();
+    if(input != ""){
+        try{
+            let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${input}`);
+            let poke = await response.json();
+            //let pokeId = poke.id;
+            console.log(poke);
+           /* let titleName = document.getElementById('result_name');
+            let photoPoke = document.getElementById('result_img');
+            photoPoke.setAttribute('src', `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokeId}.svg`);
+            titleName.innerText = poke.name;*/
+        }
+        catch(error){
+            console.log('Sorry... No Pokemon with that name!');
+        }        
+    }
+    else throw new Error('Don\'t forget to enter something...');
 })
